6f3f970cc1b2e69d1f254f271517c7feed531b24
Anton <voodoo6991@gmail.com>
2022-10-06 17:35:59 +0300

    Добавлено решение задачи № 11

---
+++ ex.11/Program.cs
@@ -0,0 +1,19 @@
+﻿// 11 - Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите первое число : ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число для проверки кратности первому: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+
+if(n >= a && n % a == 0 && a > 0)
+    Console.WriteLine(n + " кратно " + a);
+
+else if (n >= a && n % a != 0 && a > 0)    
+{
+    Console.WriteLine(n + " некратно " + a);
+    Console.WriteLine("Остаток " + n % a); 
+} 
+else
+{
+    Console.WriteLine("Проверка не может быть выполнена!");
+}
