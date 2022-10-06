54c5ebff5a745f32ea9f1dbbdd0531f43cf8ba3d
Anton <voodoo6991@gmail.com>
2022-10-06 16:43:12 +0300

    Добавлено решение задачи № 10

---
+++ ex.10/Program.cs
@@ -0,0 +1,21 @@
+﻿// 10 - Удалить вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите трёхзначное число ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int first = n / 100; 
+int second = (n / 10) % 10;
+int third = n % 10;
+
+if(n < 1000 && n > 99) 
+{  
+    // Необязательные строки, для проверки
+    Console.WriteLine(first);
+    Console.WriteLine(second);
+    Console.WriteLine(third);
+
+    Console.WriteLine(("Конечное число "  + first) + third);
+} 
+else
+{ 
+    Console.WriteLine("Не является трёхзначным");
+}
