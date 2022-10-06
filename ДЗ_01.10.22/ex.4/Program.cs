3b3308cbb6bc761e13516c5a0a390c403dceeb21
Anton <voodoo6991@gmail.com>
2022-10-06 18:57:25 +0300

    переместил файлы от 01.10.22 в новую папку

---
+++ ДЗ_01.10.22/ex.4/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4 - Выяснить является ли число чётным
+
+Console.WriteLine("Enter a number");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine(n%2==0 ? "Чётное" : "Нечётное");
---
+++ ДЗ_01.10.22/ex.9/Program.cs
@@ -0,0 +1,31 @@
+﻿// 9 - Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+Console.WriteLine("Введите число от 10 до 99");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int first = n / 10; 
+int second = n % 10;
+int max = first;
+
+if(n >= 10 && n<=99)
+{
+    if(second > first) max = second; 
+        Console.WriteLine("Наибольшая цифра числа - " + max);
+}
+else
+{
+    Console.WriteLine("Введенное число не находится в необходимом пределе");
+}
+
+/* Второй вариант решения
+
+Console.WriteLine("Введите число от 10 до 99");
+String n = Console.ReadLine() ?? "0";
+
+if(n[0] > n[1])
+{
+    Console.WriteLine("Наибольшая цифра числа - " + n[0]);
+}
+else 
+{
+   Console.WriteLine("Наибольшая цифра числа - " + n[1]);
+} */
