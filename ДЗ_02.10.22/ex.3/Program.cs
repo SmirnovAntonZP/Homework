55a079e8a1029b5de5f1ad8a99cd6d9a45396242
Anton <voodoo6991@gmail.com>
2022-10-08 09:25:28 +0300

    Добавлено решение задачи № 3

---
+++ ДЗ_02.10.22/ex.3/Program.cs
@@ -0,0 +1,26 @@
+﻿// 3 - Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите координату X: ");
+int x = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine() ?? "0");
+
+if ((x > 0) && (y > 0) && (x != 0) && (y != 0))
+{
+    Console.WriteLine("1 четверть");
+} 
+else if ((x < 0) && (y > 0) && (x != 0) && (y != 0))
+{
+    Console.WriteLine("2 четверть");
+}
+else if ((x < 0) && (y < 0) && (x != 0) && (y != 0))
+{
+    Console.WriteLine("3 четверть");
+}
+else if ((x > 0) && (y < 0) && (x != 0) && (y != 0))
+{
+    Console.WriteLine("4 четверть");
+}
+else
+{
+    Console.WriteLine("Невозможно определить четверть, Х или Y = 0");
+}
