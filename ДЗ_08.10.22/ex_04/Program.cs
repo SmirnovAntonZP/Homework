a54afc1ec92dd64b36a97458e900448d02597b58
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-12 21:28:18 +0300

    Добавлено решение для  задачи № 4

---
+++ ДЗ_08.10.22/ex_04/Program.cs
@@ -0,0 +1,21 @@
+﻿// 4 - Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число: ");
+double n = double.Parse(Console.ReadLine() ?? "0");
+double count = 1;
+
+if (n >= 1)
+{
+    while (count <= n)
+    {
+        double result = Math.Pow(count, 3);
+        if (result % 2 == 0)
+        {
+            Console.WriteLine(result);
+            count++;
+        }
+        else
+            count++;
+    }
+}
+else
+Console.WriteLine("Введенное число больше либо равно 0");
