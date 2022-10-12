d199c807f548603f065f875bc4a5b911a096b72f
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-12 19:23:20 +0300

    Добавлено решение для задачи № 2

---
+++ ДЗ_08.10.22/ex_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2 - Подсчитать сумму цифр в числе
+
+Console.Write("Введите число : ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+while (n > 0)
+{
+    int digit = n % 10;
+    sum = sum + digit;
+    n = n / 10;
+}
+
+System.Console.WriteLine("Сумма всех цифр числа = " + sum);
+
