c3134b215ac4130469b1bfaa95e01cdc3b036d5c
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-12 19:03:09 +0300

    Добавлено решение задачи № 1

---
+++ ДЗ_08.10.22/ex_01/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1 - Найти кубы чисел от 1 до N
+Console.Write("Введите число: ");
+double n = double.Parse(Console.ReadLine() ?? "0");
+double count = 1;
+
+if (n > 0)
+{
+    while (count <= n)
+    {
+        double result = Math.Pow(count, 3);
+        Console.WriteLine(count + " ^ 3 = " + result);
+        count++;
+    }
+}
+
+else 
+{
+    Console.WriteLine("Введенное число < 0");
+}
