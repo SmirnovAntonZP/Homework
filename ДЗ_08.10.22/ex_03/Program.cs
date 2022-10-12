5fa5540466d4a86b3d43af87a21080b20ca7ff00
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-12 21:07:12 +0300

    Добавлено решение для задачи № 3

---
+++ ДЗ_08.10.22/ex_03/Program.cs
@@ -0,0 +1,12 @@
+﻿// 3 - Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+int Fact = 1;
+
+for (int count = 1; count <= n; count++)
+{
+    Fact = Fact * count;
+}
+
+Console.WriteLine("Произведение чисел от 1 до " + n + " равно " + Fact);
