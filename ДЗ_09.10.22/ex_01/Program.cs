8d883a27d917d935aacd3b7713c472f96a287aa8
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-14 19:42:07 +0300

    Добавлено решение к задаче № 1

---
+++ ДЗ_09.10.22/ex_01/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1 - Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран.
+
+int Numbers = 8; 
+
+int[] FillArray(int x)
+{
+    int[] arr = new int[x];
+    for(int i = 0; i < x; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++ )
+    {
+        Console.Write($"{array[i]}");
+    }
+}
+
+PrintArray(FillArray(Numbers));
+Console.WriteLine();
