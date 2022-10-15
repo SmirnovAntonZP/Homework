5c21a3cc410535dc8dd0ab1325f564b0b87cabfa
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-15 11:28:38 +0300

    Добавлено решение для задачи № 4

---
+++ ДЗ_09.10.22/ex_04/Program.cs
@@ -0,0 +1,33 @@
+﻿// 4. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99].
+
+Console.WriteLine("Введите минимальное число для заполнения массива: ");
+int MinNumb = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите максимальное число для заполнения массива: ");
+int MaxNumb = int.Parse(Console.ReadLine() ?? "0");
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(MinNumb, MaxNumb);
+    }
+}
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+}
+
+int[] array = new int[123];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int AmountElements = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] >= 10 && array[i] <= 99)
+        AmountElements++;
+}
+Console.WriteLine("Количество элементов из отрезка [10,99] : " + AmountElements);
