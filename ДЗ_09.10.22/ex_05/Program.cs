adf360e1cdba81d74df1f61a1f84575e40edc69a
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-16 11:27:02 +0300

    Добавлено решение для задачи № 5

---
+++ ДЗ_09.10.22/ex_05/Program.cs
@@ -0,0 +1,35 @@
+﻿// 5. Найти сумму чисел одномерного массива стоящих на нечетной позиции.
+Console.Write("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальное значение: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите максимальное значение: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+void FillArray(int[]array)
+{
+    for(int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(min,max);
+    }
+}
+void PrintArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+}
+int[] array = new int[N];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int OddNumSum = 0;
+for(int i = 1; i < array.Length; i++)
+{
+    if(i % 2 == 1)
+    {
+        OddNumSum = OddNumSum + array[i];
+    }
+}
+Console.WriteLine($"Сумма элементов на нечетных позициях = {OddNumSum}");
