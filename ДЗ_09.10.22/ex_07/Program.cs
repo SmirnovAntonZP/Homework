75825ce7aca2822c9ead413bfdbf3b6848a3cbd8
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-16 12:16:07 +0300

    Добавлено решение для задачи № 7

---
+++ ДЗ_09.10.22/ex_07/Program.cs
@@ -0,0 +1,43 @@
+﻿// 7. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+
+Console.Write("Введите количество элементов массива: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите минимальное значение: ");
+int min = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите максимальное значение: ");
+int max = int.Parse(Console.ReadLine() ?? "0");
+void FillArray(int[] array)
+{
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+}
+int[] array = new int[N];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int DiffMaxMin(int[] array)
+{
+    int MaxNum = array[0]; 
+    int MinNum = array[0];
+    int Diff = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > MaxNum) MaxNum = array[i];
+        if (array[i] < MinNum) MinNum = array[i];
+        Diff = MaxNum - MinNum;
+    }
+    return Diff;
+}
+
+Console.WriteLine($"Разница между максимальным и минимальным числом {DiffMaxMin(array)}");
+Console.WriteLine();
