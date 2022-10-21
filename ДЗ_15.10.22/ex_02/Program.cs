69f6037fb653e9ec22f691d51b42ad6da40cdebf
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-21 18:52:20 +0300

    Добавлено решение для задачи № 2

---
+++ ДЗ_15.10.22/ex_02/Program.cs
@@ -0,0 +1,32 @@
+﻿//2 - Написать программу масштабирования фигуры
+
+Console.Write("Задайте координаты фигуры через ',': ");
+string[] Numb = (Console.ReadLine() ?? "0").Split(',');
+Console.WriteLine();
+Console.Write("Введите коэфициент масштабирования: ");
+double k = double.Parse(Console.ReadLine() ?? "0");
+
+double[] CoordArray(string[] Numb, double k)
+{
+    double[] array = new double[Numb.Length];
+    for (int i = 0; i < Numb.Length; i++)
+    {
+        array[i] = (double.Parse(Numb[i])) * k;
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        Console.Write($"({array[i]}, {array[i+1]})  ");
+    }
+}
+
+Console.WriteLine();
+double[] ScaleCoord = CoordArray(Numb, k);
+Console.WriteLine();
+PrintArray(ScaleCoord);
+Console.WriteLine();
+
