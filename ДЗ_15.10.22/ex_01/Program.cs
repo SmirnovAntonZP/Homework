f15b40d2491c06a79d158eb92ad6c78c8387f003
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-21 14:12:22 +0300

    Добавлено решение для задачи № 1

---
+++ ДЗ_15.10.22/ex_01/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Найти точку пересечения двух прямых
+уравнение y = k1 * x + b1
+          y = k2 * x + b2
+          b1,k1,b2,k2 - заданы
+
+          k1 * x + b1 = k2 * x + b2 
+          (k1 - k2) * x = b2 - b1 
+          x = (b2 - b1) / (k1 - k2) 
+          y = k1 * x + b1
+*/
+
+Console.WriteLine("Задайте координату k1 : ");
+double k1 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте координату b1 : ");
+double b1 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте координату k2 : ");
+double k2 = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте координату b2 : ");
+double b2 = double.Parse(Console.ReadLine() ?? "0");
+
+double x = (b2-b1)/(k1-k2);
+double y = k1*((b2-b1)/(k1-k2))+b1;
+
+Console.WriteLine($"Точка пересечения прямых имеет координаты ({x}, {y})");
