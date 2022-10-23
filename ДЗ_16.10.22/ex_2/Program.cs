034824e8712c3dcf7f46fb4e8fb550fe792089ce
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-23 04:43:01 +0300

    Добавлено решение для задачи № 2

--- ДЗ_16.10.22/ex_2/Program.cs
+++ ДЗ_16.10.22/ex_2/Program.cs
@@ -1,2 +1,38 @@
 ﻿// 2 - Задать двумерный массив следующим правилом: Aₘₙ = m+n
-Console.WriteLine("Hello, World!");
+
+Console.Write("Введите количество строк матрицы (m): ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество столбцов матрицы (n): ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+
+double[,] Array = new double[m, n]; 
+
+void FillArray(double[,] Array)  
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            Array[i,j] = (i + j);
+        }
+    }
+}
+
+void PrintArray(double[,] Array)
+{
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for(int j = 0; j < Array.GetLength(1); j++ )
+        {
+            Console.Write($" {Array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+FillArray(Array);
+Console.WriteLine();
+PrintArray(Array);
+
