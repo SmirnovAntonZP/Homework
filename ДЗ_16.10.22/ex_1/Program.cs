3a911bd50035a1a2c91f085fa884039242b842e5
SmirnovAntonZP <voodoo6991@gmail.com>
2022-10-23 04:21:58 +0300

    Изменён тип данных на double

--- ДЗ_16.10.22/ex_1/Program.cs
+++ ДЗ_16.10.22/ex_1/Program.cs
@@ -5,7 +5,18 @@ int m = int.Parse(Console.ReadLine() ?? "0");
 Console.Write("Введите количество столбцов матрицы (n): ");
 int n = int.Parse(Console.ReadLine() ?? "0");
 
-double[,] RandomArray = new double[m, n];
+double[,] RandomArray = new double[m, n]; 
+Random random = new Random();
+void FillArray(double[,] RandomArray)  
+{
+    for (int i = 0; i < RandomArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < RandomArray.GetLength(1); j++)
+        {
+            RandomArray[i,j] = (random.NextDouble() + random.Next(0,10));
+        }
+    }
+}
 
 void PrintArray(double[,] RandomArray)
 {
