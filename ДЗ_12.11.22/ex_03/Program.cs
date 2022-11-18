edb7e1f2cc83fd64335875d64bbb24e269bd351d
SmirnovAntonZP <voodoo6991@gmail.com>
2022-11-18 23:54:06 +0300

    Добавлено решение задачи № 3

---
+++ ДЗ_12.11.22/ex_03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+int result = Accerman(n, m);
+
+int Accerman(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Accerman(n - 1, 1);
+    else
+        return Accerman(n - 1, Accerman(n, m - 1));
+}
+Console.WriteLine();
+Console.WriteLine($"Функция Аккермана для чисел {n},{m} = {result}");
+Console.WriteLine();
