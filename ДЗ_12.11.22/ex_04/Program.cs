86187c164aa1edc4278bbf8e25a02e10e62aba03
SmirnovAntonZP <voodoo6991@gmail.com>
2022-11-19 00:07:38 +0300

    Добавлено решение для задачи № 4

---
+++ ДЗ_12.11.22/ex_04/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Написать программу показывающие первые N чисел, 
+для которых каждое следующее равно сумме двух предыдущих. 
+Первые два элемента последовательности задаются пользователем */
+
+Console.Write("Введите первое число: ");
+int numA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int numB = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите количество чисел для показа: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+void NumSum(int a, int b, int n, int x)
+{
+    if (n <= x)
+    {
+        int sum = a + b;
+        Console.Write($"{sum} ");
+        NumSum(b,sum,n+1,x);
+        
+    }
+}
+
+NumSum(numA, numB, 1, N);
