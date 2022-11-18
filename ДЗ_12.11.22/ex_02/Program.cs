8334290874a5e4de3f6d81f5f0fe9ef91477a5f4
SmirnovAntonZP <voodoo6991@gmail.com>
2022-11-18 23:19:21 +0300

    Добавлено решение для Задачи № 2

---
+++ ДЗ_12.11.22/ex_02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+
+int SumNumbers(int a, int b)
+{
+    if (a == b) return b;
+    return a + SumNumbers(a + 1, b);
+}
+
+if (M > N) Console.WriteLine($"сумма элементов = " + SumNumbers(N, M));
+    else Console.WriteLine($"сумма элементов = " + SumNumbers(M, N));
+
+if (M == N) Console.WriteLine(M + " = " + N);
