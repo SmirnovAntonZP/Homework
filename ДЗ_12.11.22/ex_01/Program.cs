8f3cc87e84257087c03dad15c91fe68a6fed219b
SmirnovAntonZP <voodoo6991@gmail.com>
2022-11-18 22:31:36 +0300

    Добавлено решение для задачи № 1

---
+++ ДЗ_12.11.22/ex_01/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+
+void PrintNumbers(int a, int b)
+{
+    if(a >= b)
+    {
+        Console.Write(a+" ");
+        PrintNumbers(a-1, b);
+    }
+}
+
+PrintNumbers(M , N);
+Console.WriteLine();
